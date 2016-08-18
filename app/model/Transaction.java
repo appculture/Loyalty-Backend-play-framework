e1a9023ec8e20213378c1b256625c929b391a381
Aleksandar Bozic <aleksandar.bozic@appculture.com>
2016-08-18 16:03:30 +0200

    Added model clases (basic setup).

---
+++ app/model/Transaction.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.Date;
+
+/**
+ * Transaction represent some action which changed {@link Customer} points balance.
+ * Transaction could be purchase or redeem.
+ * <p>
+ * Created by abozic on 8/18/16.
+ */
+public class Transaction {
+
+    private String transationId;
+    private Date date;
+    private String description;
+    private double points;
+    //not in use for a moment
+    //private TransactionType type;
+    private Customer customer;
+
+    public String getTransationId() {
+        return transationId;
+    }
+
+    public void setTransationId(String transationId) {
+        this.transationId = transationId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPoints() {
+        return points;
+    }
+
+    public void setPoints(double points) {
+        this.points = points;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
