6c6a3de1ac90d7846e564aa45c48a5de217801c6
Aleksandar Bozic <aleksandar.bozic@appculture.com>
2016-08-18 16:03:30 +0200

    Added model clases (basic setup).

---
+++ app/model/Configuration.java
@@ -0,0 +1,37 @@
+package model;
+
+/**
+ * Configuration represent system default configuration.
+ * <p>
+ * Created by abozic on 8/18/16.
+ */
+public class Configuration {
+
+    private String currency;
+    private double bonusPointsRation;
+    private boolean confirmPurchase;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public double getBonusPointsRation() {
+        return bonusPointsRation;
+    }
+
+    public void setBonusPointsRation(double bonusPointsRation) {
+        this.bonusPointsRation = bonusPointsRation;
+    }
+
+    public boolean isConfirmPurchase() {
+        return confirmPurchase;
+    }
+
+    public void setConfirmPurchase(boolean confirmPurchase) {
+        this.confirmPurchase = confirmPurchase;
+    }
+}
---
+++ app/model/Customer.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.util.Date;
+
+/**
+ * Customer contains all profile information's related to actual client.
+ * <p>
+ * Created by abozic on 8/18/16.
+ */
+public class Customer {
+
+    private String customerId;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private double points;
+    private Date birthDate;
+    /**
+     * User ID represent external system ID, ID might be filled during import of the data.
+     */
+    private String userId;
+    private Date creationDate;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public double getPoints() {
+        return points;
+    }
+
+    public void setPoints(double points) {
+        this.points = points;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
