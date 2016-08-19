343a0c714215230d4e741c3e3e8e3957e27a3ae5
Aleksandar Bozic <aleksandar.bozic@appculture.com>
2016-08-19 12:09:18 +0200

    Added User model.

---
+++ app/models/User.java
@@ -0,0 +1,45 @@
+package models;
+
+/**
+ * User of the system.
+ * Created by abozic on 8/19/16.
+ */
+public class User {
+
+    private String fullName;
+    private String username;
+    private String password;
+    private UserType type;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+}
