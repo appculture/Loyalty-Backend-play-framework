a52b8c4b4718022be7a5880339108f37119518f3
Aleksandar Bozic <aleksandar.bozic@appculture.com>
2016-08-19 17:21:07 +0200

    TESTING enhanced issue: temporary disabled enhanced plugin, commented some code in SampleDBController.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,11 +3,25 @@
 
 # --- !Ups
 
+create table customer (
+  customer_id                   varchar(255) not null,
+  full_name                     varchar(255) not null,
+  email                         varchar(255),
+  phone_number                  varchar(255),
+  points                        double,
+  birth_date                    timestamp,
+  user_id                       varchar(255),
+  creation_date                 timestamp,
+  constraint uq_customer_email unique (email),
+  constraint pk_customer primary key (customer_id)
+);
+
 create table test (
-  id                            bigserial not null,
+  id                            bigint not null,
   name                          varchar(255),
   constraint pk_test primary key (id)
 );
+create sequence test_seq;
 
 create table public.user (
   username                      varchar(255) not null,
@@ -21,7 +35,10 @@ create table public.user (
 
 # --- !Downs
 
-drop table if exists test cascade;
+drop table if exists customer;
 
-drop table if exists public.user cascade;
+drop table if exists test;
+drop sequence if exists test_seq;
+
+drop table if exists public.user;
 
