45612d86685c0f3a2fb8a5acbe0c8cb3f0ccb075
Aleksandar Bozic <aleksandar.bozic@appculture.com>
2016-08-23 17:33:14 +0200

    Added vocher sample data.

--- conf/evolutions/default/sample_data.sql
+++ conf/evolutions/default/sample_data.sql
@@ -29,3 +29,12 @@ VALUES (uuid_generate_v4(), 'Purchase transaction #1', 100, (SELECT customer_id
 INSERT INTO public.user (username, fullname, password, type)
 VALUES ('pera@pera.com', 'Pera Peric', '$2a$06$XFRi7iKwKbvoar/gAUykBebYyWMbMRR3plBvZCQU1dOapyLqXlALG', 'ADMIN');
 
+
+/******* user ********/
+INSERT INTO voucher (voucher_id, name, description, points, active, expiry_date)
+VALUES (nextval('voucher_seq'), 'Sample voucher #1', 'This voucher will provider 20% discount', 130, TRUE,
+        now() + INTERVAL '1 day'),
+  (nextval('voucher_seq'), 'Sample voucher #2', 'This voucher will provider 50% discount', 500, TRUE,
+   now() + INTERVAL '2 day'),
+  (nextval('voucher_seq'), 'Sample voucher #3', 'This voucher will provider CHF 50 valued gift', 200, TRUE,
+   now() + INTERVAL '10 day');
